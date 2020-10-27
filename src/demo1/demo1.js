26d22176d5d014c22345ce1badc905e77b01a201
梁敏 <liangm@gzjp.cn>
2020-10-27 11:32:40 +0800

    feat:第一课学习完成

--- src/demo1/demo1.js
+++ src/demo1/demo1.js
@@ -1,3 +1,15 @@
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn")
 //在渲染进程里面的窗口只能使用require('electron').remote.BrowserWindow 得到
-const BrowserWindow = require('electron').remote.BrowserWindow;
+const BrowserWindow = require('electron').remote.BrowserWindow
+const path = require('path')
+btn.onclick=()=>{
+  newWin = new BrowserWindow({
+    width:300,
+    height:350,
+  })
+  newWin.loadFile(path.join(__dirname,'new.html'))
+  newWin.on('closed',()=>{
+    newWin = null
+  })
+}
+
--- src/indexL.js
+++ src/indexL.js
@@ -6,13 +6,26 @@ var app = electron.app;
 //引用窗口
 var BrowserWindow = electron.BrowserWindow;
 
+//引用路劲
+var path = require('path')
+
 //声明要打开的主窗口
 var mainWindow = null;
 
 //监听ready事件 创建窗口
 app.on('ready',()=>{
-  mainWindow- new BrowserWindow({height:200,width:400});
-  mainWindow.loadFile('index.html');
+  mainWindow = new BrowserWindow(
+    {
+      height:200,
+      width:400,
+      //必须添加此行才能在渲染进程中使用nodejs
+      webPreferences:{
+        nodeIntegration:true,
+        enableRemoteModule:true
+      }
+    }
+  );
+  mainWindow.loadFile(path.join(__dirname,'demo1/demo1.html'));
   mainWindow.on('closed',()=>{
     mainWindow = null;
   })
